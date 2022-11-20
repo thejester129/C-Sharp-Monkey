5a5dcacf63b62cecc4cd146be93668286b2de228
thejester129 <thejester129@gmail.com>
2022-11-20 14:26:48 +0000

    Token parsing repl

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,3 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using C_Sharp_Monkey;
+
+Repl.Start();
--- src/Repl.cs
+++ src/Repl.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 namespace C_Sharp_Monkey
 {
-    public class Repl
+    public static class Repl
     {
-        public Repl()
+        public static void Start()
         {
+            while (true)
+            {
+                Console.Write("# ");
+
+                string? line = Console.ReadLine();
+
+                if (line == null)
+                {
+                    continue;
+                }
+
+                if (line == "exit")
+                {
+                    break;
+                }
+
+                var lexer = new Lexer(line);
+
+                var nextToken = lexer.NextToken();
+
+                while (nextToken != null && nextToken.TokenType != TokenType.EOF)
+                {
+                    Console.WriteLine("Parsed " + nextToken.Literal + " as " + nextToken.TokenType);
+                    nextToken = lexer.NextToken();
+                }
+            }
         }
     }
 }
